7cce652d0ddf04574253ca04e8ba30c06059caa6
Andrew Hoover <32847250+AndrewHoover@users.noreply.github.com>
2024-05-17 21:51:13 -0500

    Update Omnibot_5402_remote_1.ino

--- Omnibot_5402_remote_1/Omnibot_5402_remote_1.ino
+++ Omnibot_5402_remote_1/Omnibot_5402_remote_1.ino
@@ -2,16 +2,19 @@
 #include <WiFi.h>
 #include <math.h>
 
-
 // Pin Definitions
-static const int joy1XPin = 35;
-static const int joy1YPin = 34;
+static const int joy1XPin = 34;
+static const int joy1YPin = 35; // uncomment if using the HiLetgo ESP32
+// static const int joy1YPin = 39; // uncomment if using the Adafruit HUZZAH32 which has less I/O
 static const int joy2XPin = 36;
+static const int leftArmPin = 32;
+static const int rightArmPin = 33;
+static const int connectedPin = 25;
 
 // Trim adjustments for non linear crap pots. #####Note to create auto trim on startup in future
-const int joy1XTrim = 90;
-const int joy1YTrim = 30;
-const int joy2XTrim = 20;
+const int joy1XTrim = 170;
+const int joy1YTrim = 130;
+const int joy2XTrim = 120;
 
 float x;      // Joystick 1 X axis mapped as floating point decimal -1 to 1
 float y;      // Joystick 1 Y axis mapped as floating point decimal -1 to 1
@@ -24,7 +27,7 @@ double Max;   // Tracker for max angle value to prevent square joystick values
 double power; // Amount of difference between polar center and joystick position - used to calculate speed in any direction
 
 // Receiver MAC address for espNow communication
-uint8_t receiverMacAddress[] = {0x94,0xB9,0x7E,0xD4,0x0D,0x58};  //94:B9:7E:D4:0D:58
+uint8_t receiverMacAddress[] = {0x9C,0x9C,0x1F,0xC7,0xA7,0xB4};  //9C:9C:1F:C7:A7:B4
 
 // espNow packet data structure
 struct PacketData
@@ -33,8 +36,13 @@ struct PacketData
   double rightfront;  // Calculated direction and speed for Right Front wheel (Negative values reverse -1 to 0; forward values are positive 0 to 1)
   double leftrear;    // Calculated direction and speed for Left Rear wheel (Negative values reverse -1 to 0; forward values are positive 0 to 1)
   double rightrear;   // Calculated direction and speed for Right Rear wheel (Negative values reverse -1 to 0; forward values are positive 0 to 1)
+  int leftarm;        // Current position of the left arm shoulder servo
+  int rightarm;       // Current position of the left arm shoulder servo
 };
-PacketData data; 
+
+// Create Objects
+PacketData data;
+
 
 // callback when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
@@ -48,6 +56,10 @@ void setup()
 {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
+
+  // Set I/O pins
+  pinMode(connectedPin,OUTPUT);
+  digitalWrite(connectedPin,LOW);
 
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK) 
@@ -73,22 +85,30 @@ void setup()
   if (esp_now_add_peer(&peerInfo) != ESP_OK)
   {
     Serial.println("Failed to add peer");
+    digitalWrite(connectedPin,LOW);
     return;
   }
   else
   {
     Serial.println("Succes: Added peer");
+    digitalWrite(connectedPin,HIGH);
   } 
      
 }
  
 void loop() {
 
-    // Get analog values and map to floats between -1 and 1
+  data.leftarm = map(analogRead(leftArmPin), 0, 4096, 30, 150);
+  data.rightarm = map(analogRead(rightArmPin), 0, 4096, 150, 30);
+  //Serial.println(data.rightarm);
+
+  // Get analog values and map to floats between -1 and 1
   x = float(map(analogRead(joy1XPin)+joy1XTrim, 0, 4095, -1024, 1024)) / float(1024);
   y = float(map(analogRead(joy1YPin)+joy1YTrim, 0, 4095, -1024, 1024)) / float(1024);
   turn = float(map(analogRead(joy2XPin)+joy2XTrim, 0, 4095, -1024, 1024)) / float(1024);
-  Serial.println(turn);
+  // Serial.println(x);
+  Serial.println(y);
+  // Serial.println(turn);
   
   // Base calculations
   theta = atan2(y,x);
@@ -125,11 +145,13 @@ void loop() {
   if (result == ESP_OK) 
   {
     //Serial.println("Sent with success");
+    digitalWrite(connectedPin,HIGH);
   }
   else 
   {
-    Serial.println("Error sending the data");
-  }    
+    //Serial.println("Error sending the data");
+    digitalWrite(connectedPin,LOW);
+  } 
   
   delay(10);
 }
